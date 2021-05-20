e28ba1161071732c1e3b838010548095fd0671b9
gdhar92 <gdhar@chicagobooth.edu>
2021-05-20 06:08:56 +0000

    Class assignment complete now

--- app/models/character.rb
+++ app/models/character.rb
@@ -10,13 +10,29 @@
 #  movie_id   :integer
 #
 class Character < ApplicationRecord
+  # def movie
+  #   my_movie_id = self.movie_id
+
+  #   matching_movies = Movie.where({ :id => my_movie_id })
+
+  #   the_movie = matching_movies.at(0)
+
+  #   return the_movie
+  # end
+
   def movie
-    my_movie_id = self.movie_id
-
-    matching_movies = Movie.where({ :id => my_movie_id })
-
-    the_movie = matching_movies.at(0)
-
-    return the_movie
+    my_parent_id = self.movie_id
+    matching_records = Movie.where({ :id => my_parent_id})
+    the_parent = matching_records.at(0)
+    return the_parent
   end
+
+  def actor
+    my_parent_id = self.actor_id
+    matching_records = Actor.where({ :id => my_parent_id})
+    the_parent = matching_records.at(0)
+    return the_parent
+  end
+
+  belongs_to(:movie, { :foreign_key => "movie_id", :class_name => "Movie"})
 end
